1918ecbfac3275865e6d9cab6ffd7de200e7024d
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-20 22:53:44 -0500

    added structure for ml program

--- Program.cs
+++ Program.cs
@@ -6,9 +6,42 @@ namespace c_sharp_machine_learning
 {
     class Program
     {
+        /// <summary>
+        /// IrisData is the model that shows the structure of the data schema.
+        /// </summary>
+        public class IrisData
+        {
+            [LoadColumn(0)]
+            public float SepalLength;
+
+            [LoadColumn(1)]
+            public float SepalWidth;
+
+            [LoadColumn(2)]
+            public float PetalLength;
+
+            [LoadColumn(3)]
+            public float PetalWidth;
+
+            [LoadColumn(4)]
+            public string Label;
+        }
+
+        /// <summary>
+        /// IrisPrediction will return the predicted result.
+        /// </summary>
+        public class IrisPrediction
+        {
+            [Column("PredeictedLabel")]
+            public string PredictedLabel;
+        }
+
         static void Main(string[] args)
         {
-           
+            // Instantizing
+            var mlContext = new MLContext();
+
+            
         }
     }
 }
